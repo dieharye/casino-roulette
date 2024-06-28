4a285836a6f4306006c4a3acd924e40713295232
black-rust <blackrust1030@gmail.com>
2024-06-28 23:29:38 +0900

    middlewares fixed

--- app/middleware/admin.js
+++ app/middleware/admin.js
@@ -2,13 +2,13 @@ module.exports = function () {
 	var module = {};
 
 	module.login = function ( req, res, next ) {
-		next()
-		// if ( req.session.admin ) {
-		// 	next();
-		// } else {
-		// 	req.flash( 'error', "Please log in to Play" );
-		// 	res.redirect( '/backend' );
-		// }
+		// next()
+		if ( req.session.admin ) {
+			next();
+		} else {
+			req.flash( 'error', "Please log in to Play" );
+			res.redirect( '/backend' );
+		}
 	};
 
 	module.isLogin = function ( req, res, next ) {
